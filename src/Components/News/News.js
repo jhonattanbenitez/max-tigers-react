a32a729f432d513c0b5c463acfd87d834eb9d933
jhonattanbenitez <jhonattan.benitez@hotmail.com>
2018-12-11 23:43:04 -0500

    news

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Col} from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Col, Button} from 'reactstrap';
 import  './news.css';
 
-
-
-const News = ({title, content, image, }) => {
-  
-   return (
-                 
-               
-                <Col xs="12" sm="4">
+const handleLinks = () =>{
+  console.log("link clicked");
+}
+/* = ({title, content, image }) => */
+class News extends Component  {
+   
+   /*  state = { showAll: false}
+    showMore = () => this.setState({showAll: true});
+    showLess = () => this.setState({showAll: false}); */
+    render(){
+      const {title, content, image} = this.props;
+      /* const {showAll} = this.state;
+      const limit = 250;
+      if(content.lenght <= limit){
+        return (<Col xs="12" sm="4">
                   <Card className="cardproperties">
                     <CardImg top width="100%" src={image} alt="Card image cap" />
                     <CardBody>
@@ -18,8 +25,58 @@ const News = ({title, content, image, }) => {
                     </CardBody>
                   </Card>
                 </Col>
-    
-  );
+        );
+
+      }
+      if(showAll){
+        return(
+          <Col xs="12" sm="4">
+          <Card className="cardproperties">
+            <CardImg top width="100%" src={image} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{title}</CardTitle>
+              <CardText>{content}</CardText>
+              <Button onClick={this.showLess}>Ver menos</Button>
+            </CardBody>
+          </Card>
+        </Col>
+        );
+       
+
+      }
+      const toShow = content.substring(0, limit)+"...";
+      return(
+        <Col xs="12" sm="4">
+          <Card className="cardproperties">
+            <CardImg top width="100%" src={image} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{title}</CardTitle>
+              <CardText>{content}</CardText>
+              {toShow}
+          <Button onClick={this.showMore}>Ver más</Button>
+            </CardBody>
+          </Card>
+        </Col>
+      ); */
+      return (
+                 
+               
+        <Col xs="12" sm="4">
+          <Card className="cardproperties">
+            <CardImg top width="100%" src={image} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{title}</CardTitle>
+              <CardText>{content}</CardText>
+              <Button onClick={handleLinks}>Ver más</Button>
+            </CardBody>
+          </Card>
+        </Col>
+
+      );
+
+    } 
+ 
+   
 };
 
 export default News;
